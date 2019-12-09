f1aabe3209c89e3f7257337043e694620df1b2e2
Grant Murphy <grantsmurphy@gmail.com>
2019-12-09 12:29:55 -0500

    check for prooper noun

--- load_dict.py
+++ load_dict.py
@@ -7,13 +7,15 @@ print(len(data),type(data))
 #My way
 def read_data(word):
     if word in data:
+        word.lower()
         if len(data[word]) > 1:
             return ',\n '.join(data[word])
         print(type(data[word]))
         return data[word][0]
+    elif word.capitalize() and data[word].capitalize():
+        return data[word][0]    
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no. " % get_close_matches(word, data.keys())[0])
-        yn.lower()
         if yn == "y":
             return data[get_close_matches(word, data.keys())[0]]
         elif yn == "n":
@@ -25,17 +27,17 @@ def read_data(word):
             
 
 #The instuctors way
-def translate(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead? " % get_close_matches(word, data.keys())[0]    
-    else:
-        return "The word does not exist."
+# def translate(word):
+#     word = word.lower()
+#     if word in data:
+#         return data[word]
+#     elif len(get_close_matches(word, data.keys())) > 0:
+#         return "Did you mean %s instead? " % get_close_matches(word, data.keys())[0]    
+#     else:
+#         return "The word does not exist."
 
 
 while True:
-    word = input("Please input a word, To Stop the program type Stop This: ").lower()
+    word = input("Please input a word, To Stop the program type Stop This: ")
     print(read_data(word)) 
     continue
