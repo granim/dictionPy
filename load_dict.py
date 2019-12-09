e66f6ae0ac23d327053113c2f7f0408b98331e48
Grant Murphy <grantsmurphy@gmail.com>
2019-12-09 09:44:04 -0500

    added some control flow for multiple definitions

--- load_dict.py
+++ load_dict.py
@@ -2,16 +2,26 @@ import json
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
-
+print(len(data),type(data))
 
 #My way
 def read_data(word):
-        if word in data:
-            return data[word]
-        elif len(get_close_matches(word, data.keys())) > 0:
-            return "Did you mean %s instead? " % get_close_matches(word, data.keys())[0]
+    if word in data:
+        if len(data[word]) > 1:
+            return ',\n '.join(data[word])
+        print(type(data[word]))
+        return data[word][0]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no. " % get_close_matches(word, data.keys())[0])
+        yn.lower()
+        if yn == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "n":
+            return "The word doesn't exist. Please double check it."
         else:
-            return "The word does not exist"    
+            return "We did not understand your entry. "        
+    else:
+        return "The word does not exist"    
             
 
 #The instuctors way
@@ -19,6 +29,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead? " % get_close_matches(word, data.keys())[0]    
     else:
         return "The word does not exist."
 
